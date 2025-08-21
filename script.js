7323480b92c8f68e936481d4b6209ebf52acfef2
estefanyvazilina <estefany.vaz.lima@escola.pr.gov.br>
2025-08-21 19:06:10 -0300

    Aula 08

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal ({ reset: true });
+
+sr.reveal ("#image" , {duration:1000});
+
+sr.reveal ("#textoconteudo" , {
+  rotate: {x=0,y=100,z=0},
+  duration:1500});
+
+sr.reveal ("#formulario" , {duration:1000});
